4f5cfa5c23b15fe9f37e198142bd9b03a3449b0d
Moch Rizky Taufiqurrahman <moch.rizky.tif23@polban.ac.id>
2024-05-18 18:37:41 +0700

    menambahkan include body.h

--- rizky.h
+++ rizky.h
@@ -2,26 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include "body.h"
 #ifndef rizky_H
 #define rizky_H
 
-// Struct untuk barang
-typedef struct Node{
-    char info[100];
-    char detail[100];
-    float price;
-    int available;
-    struct Node *next;
-}Node;
-
-// struct untuk pointer head&tail
-typedef struct playlist {
-    Node *head;
-    Node *tail;
-} List;
-
 // fungsi yang ada
-void createList(List *myList);
-void disList(List *myList);
+void createList(List *list);
+void disList(List *list);
 
 #endif
